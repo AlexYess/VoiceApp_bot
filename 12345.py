240e650357c7da11a92690283b7ffdcd41570cd9
AlexYess <aalexesetov2541@gmail.com>
2023-06-17 20:14:29 +0300

    15:53 03.06.2023

--- 12345.py
+++
@@ -1,9 +0,0 @@
-my_dict = {1: 'one', 2: 'two', 3: 'three'}
-
-print(my_dict.keys())
-a = '2'
-
-if a in my_dict:
-    print("Число 2 есть в ключах словаря")
-else:
-    print("Число 2 отсутствует в ключах словаря")
